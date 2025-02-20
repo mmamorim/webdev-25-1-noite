486522f5d227bd17998b46260a3cae68ee10106a
mmamorim <marc.m.amorim@gmail.com>
2025-02-19 21:45:22 -0300

    v1

---
+++ Aula02-19-FEV/user.js
@@ -0,0 +1,10 @@
+
+export function createUser() {
+    let obj = {
+        firstName: "Marcelo",
+        lastName: "Amorim",
+        rg: 383487587,
+        email: "m@m.com"
+    }
+    return obj
+}
