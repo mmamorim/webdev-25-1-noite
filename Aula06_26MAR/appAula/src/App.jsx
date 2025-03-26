04dd713664c3f6bdd9b02988ac7218f529702a6b
mmamorim <marc.m.amorim@gmail.com>
2025-03-26 20:52:43 -0300

    v1

--- Aula06_26MAR/appAula/src/App.jsx
+++ Aula06_26MAR/appAula/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { Icon } from "@iconify/react";
 import { Topo } from "./components/Topo";
 import { AddTodo } from "./components/AddTodo";
 
 function App() {
+  const [ todoList, setTodoList ] = useState(["Algo","Algo","outro","terceiro"])
 
   return (
     <>
       <Topo />
       <AddTodo />
+      <div>
+        { todoList.map((task,idx) => <div key={idx}>{task}</div> ) }
+      </div>
     </>
   )
 }
--- Aula06_26MAR/appAula/src/components/AddTodo.jsx
+++ Aula06_26MAR/appAula/src/components/AddTodo.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 
 export function AddTodo() {
+    const [ taskName, setTaskName ] = useState("Algo")
+    
+    function addTask() {
+        console.log("addTask",taskName);
+    }
 
     return (
         <>
@@ -11,10 +17,10 @@ export function AddTodo() {
                 <div className="flex items-center justify-start gap-2">
                     <div>Nome:</div>
                     <div>
-                        <input type="text" className="border border-black p-2" />
+                        <input value={taskName} onChange={(e) => setTaskName(e.target.value)} type="text" className="border border-black p-2" />
                     </div>
                     <div>
-                        <button className="border border-black p-1 rounded">
+                        <button onClick={addTask} className="border border-black p-1 rounded">
                             Adicionar
                         </button>
                     </div>
