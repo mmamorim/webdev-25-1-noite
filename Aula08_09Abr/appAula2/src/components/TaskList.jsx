fff9a22c591d4c6d12b58d8f4a301bc687032f8b
mmamorim <marc.m.amorim@gmail.com>
2025-04-09 22:38:18 -0300

    v1

--- Aula08_09Abr/appAula2/src/components/TaskList.jsx
+++ Aula08_09Abr/appAula2/src/components/TaskList.jsx
@@ -1,30 +1,45 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import AddTaskBox from "./AddTaskBox";
 import TaskListItem from "./TaskListItem"
+import sgdb from "../sgdb.js"
+
+sgdb.init()
 
 export default function TaskList() {
-    const [taskList, setTaskList] = useState(["Tarefa 1", "fsfsfsf", "Tarefa 3", "Tarefa 4"])
+    const [taskList, setTaskList] = useState({})
+
+    useEffect(() => {
+        setTaskList(sgdb.getData().todos)        
+    }, [])
 
     function onAddTask(taskName) {
         console.log("chamei onTaskAdd com: ",taskName);
-        setTaskList([ ...taskList, taskName])
+        //setTaskList([ ...taskList, taskName])
+        let task = {
+            id: sgdb.newID("task"),
+            task: taskName
+        }
+        sgdb.data.todos[task.id] = task
+        sgdb.write()
+        setTaskList(sgdb.getData().todos)
     }
 
-    function onRemoveItem(idx) {
-        console.log("onRemoveItem: ",idx);
-        taskList.splice(idx,1)
-        setTaskList([...taskList])
+    function onRemoveItem(id) {
+        console.log("onRemoveItem: ",id);
+        delete sgdb.data.todos[id]
+        sgdb.write()
+        setTaskList(sgdb.getData().todos)
     }
 
     return (
         <>
             <AddTaskBox onAddTask={onAddTask} />
-            <div className="border border-black p-2 m-2 rounded">
+            <div className="border border-black p-2 m-2 rounded w-140">
                 <div>
                     tarefas
                 </div>
                 <hr className="my-2" />
-                {taskList.map((elem, idx) => <TaskListItem idx={idx} onRemove={onRemoveItem} taskName={elem} key={idx} />)}
+                {Object.keys(taskList).map((id) => <TaskListItem idx={id} onRemove={onRemoveItem} taskName={taskList[id].task} key={id} />)}
             </div>
         </>
     )
--- Aula08_09Abr/appAula2/src/components/TaskListItem.jsx
+++ Aula08_09Abr/appAula2/src/components/TaskListItem.jsx
@@ -1,4 +1,4 @@
-import { Icon } from "@iconify/react" 
+import { Icon } from "@iconify/react"
 
 export default function TaskListItem({ taskName, onRemove, idx }) {
 
@@ -6,17 +6,22 @@ export default function TaskListItem({ taskName, onRemove, idx }) {
         onRemove(idx)
     }
 
-    return(
+    return (
         <>
-            <div className="flex items-center justify-start bg-gray-300 p-2 my-1">
+            <div className="flex items-center  justify-between bg-gray-300 p-2 my-1">
                 <div>
                     {taskName}
                 </div>
-                <div className="px-1 rounded-lg pt-1 mx-2 bg-white text-green-800 text-xl">
-                    <Icon icon="mdi:check" />
-                </div>
-                <div onClick={removeItem} className="px-1 rounded-lg pt-1 mx-2 bg-white text-red-800 text-xl">
-                    <Icon icon="mdi:delete" />
+                <div className="flex items-center gap-2">
+                    <div className="px-1 rounded-lg pt-1 bg-white text-black text-xl">
+                        <Icon icon="mdi:pencil" />
+                    </div>
+                    <div className="px-1 rounded-lg pt-1 bg-white text-green-800 text-xl">
+                        <Icon icon="mdi:check" />
+                    </div>
+                    <div onClick={removeItem} className="px-1 rounded-lg pt-1 bg-white text-red-800 text-xl">
+                        <Icon icon="mdi:delete" />
+                    </div>
                 </div>
             </div>
         </>
